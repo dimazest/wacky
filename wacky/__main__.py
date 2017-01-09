986e81854287fbd1f619a552d6d61f55271135d8
Dmitrijs Milajevs <dimazest@gmail.com>
2017-01-09 16:57:13 -0500

    counters

--- wacky/__main__.py
+++ wacky/__main__.py
@@ -1,6 +1,7 @@
 import codecs
 import json
 import sys
+import gzip
 
 import click
 
@@ -26,11 +27,25 @@ def tokens(token_template):
 
 @cli.command()
 @click.option('--token-template', default=None, help='The template to render tokens. Available fields are: word, lemma, tag, feats, head and rel.')
-def sentences(token_template):
+@click.option('--index', default=None, type=click.Path(exists=True))
+@click.option('--min-count', default=None, type=int)
+def sentences(token_template, index, min_count):
+    token_index = dict()
+    with gzip.open(index, mode='rt') as f:
+        for i, line in enumerate(f, start=1):
+            item, count = line.rsplit(' ', maxsplit=1)
+            count = int(count)
+
+            if min_count is not None and count < min_count:
+                i = -i
+
+            token_index[item] = str(i)
+
     for raw_document in readers.raw_documents(sys.stdin):
         for tokens in readers.sentences(raw_document):
-
-            if token_template is None:
+            if index:
+                print(' '.join(token_index[token_template.format(**t)] for t in tokens))
+            elif token_template is None:
                 print(json.dumps(tokens))
             else:
                 print(' '.join(token_template.format(**t) for t in tokens))
@@ -41,6 +56,7 @@ def documents():
     for document in readers.documents(readers.raw_documents(sys.stdin)):
         print(json.dumps(document))
 
+
 @cli.command()
 @click.option('--most-common', default=None, type=int)
 def count(most_common):
@@ -48,7 +64,30 @@ def count(most_common):
 
     counter = Counter()
 
-    counter.update(w[:-1]for w in sys.stdin)
+    counter.update(w[:-1] for w in sys.stdin)
+
+    for item, count in counter.most_common(most_common):
+        print(item, count)
+
+
+def read_counts(input_):
+    from collections import Counter
+    counter = Counter()
+
+    for line in sys.stdin:
+        item, count = line.rsplit(' ', maxsplit=1)
+        count = int(count.strip())
+
+        counter[item] += count
+
+    return counter
+
+
+@cli.command()
+@click.argument('files', nargs=-1, type=click.Path(exists=True))
+@click.option('--most-common', default=None, type=int)
+def merge(files, most_common):
+    counter = read_counts(sys.stdin)
 
     for item, count in counter.most_common(most_common):
         print(item, count)
